bdd4def1603ee60236f364eb353f5a001ce10595
mmcevoy93 <mmcevoy@ualberta.ca>
2018-02-20 15:07:37 -0700

    blah

--- client/test_talk_max/arduino_talk.cpp
+++ client/test_talk_max/arduino_talk.cpp
@@ -5,7 +5,7 @@ void setup() {
 
   Serial.begin(9600);
 }
-
+// TODO Communicationseems to work. just need to findout if the the correct values are being sent
 int main() {
   enum State {Input_Ready, Sending_In, Waiting_N, Sending_A, Waiting_W, Ending};
   State curr_state = Sending_In;
@@ -13,17 +13,19 @@ int main() {
   Serial.println("Starting");
   char buffer[129];
   int used = 0;
-
+  int paths;
+  int output;
   while (true) {
     while (Serial.available() == 0);
     if (curr_state == Sending_In){
-      Serial.println("R 5365486 -11333915 5364728 -11335891");
+      Serial.println("R 5365486 -11333915 5364728 -11335891"); //Put input here
       Serial.flush();
       curr_state = Waiting_N;
     }
     if (curr_state == Waiting_N){
       buffer[used] = Serial.read();
       if (buffer[0] == 'N'){
+        output = Serial.read();
         Serial.write('A');
         curr_state = Waiting_W;
       }
@@ -35,15 +37,13 @@ int main() {
       }
       if (buffer[0] == 'E'){
         curr_state = Ending;
+        Serial.flush();
       }
     }
     if (curr_state == Ending){
+
       break;
     }
-
-
   }
-
-
   return 0;
 }
