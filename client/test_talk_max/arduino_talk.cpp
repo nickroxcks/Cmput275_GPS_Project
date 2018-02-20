7acca4bec5e0fb5c37a180d3694364e4b2505ea7
mmcevoy93 <mmcevoy@ualberta.ca>
2018-02-20 14:07:33 -0700

    save early save often

--- client/test_talk_max/arduino_talk.cpp
+++ client/test_talk_max/arduino_talk.cpp
@@ -7,28 +7,43 @@ void setup() {
 }
 
 int main() {
+  enum State {Input_Ready, Sending_In, Waiting_N, Sending_A, Waiting_W, Ending};
+  State curr_state = Sending_In;
   setup();
-
-  Serial.println("Hello, this is the Arduino!");
-  Serial.flush();
-
+  Serial.println("Starting");
   char buffer[129];
   int used = 0;
 
   while (true) {
     while (Serial.available() == 0);
-
-    buffer[used] = Serial.read();
-    ++used;
-
-    if (buffer[used-1] == '\n') {
-      buffer[used-1] = buffer[used] = buffer[used+1] = '.';
-      buffer[used+2] = '\n';
-      buffer[used+3] = '\0';
-      Serial.write("Test"); // does not add \r\n
-      used = 0;
+    if (curr_state == Sending_In){
+      Serial.println("R 5365486 -11333915 5364728 -11335891");
+      Serial.flush();
+      curr_state = Waiting_N;
     }
+    if (curr_state == Waiting_N){
+      buffer[used] = Serial.read();
+      if (buffer[0] == 'N'){
+        Serial.write('A');
+        curr_state = Waiting_W;
+      }
+    }
+    if (curr_state == Waiting_W){
+      buffer[used] = Serial.read();
+      if (buffer[0] == 'W'){
+          Serial.write('A');
+      }
+      if (buffer[0] == 'E'){
+        curr_state = Ending;
+      }
+    }
+    if (curr_state == Ending){
+      break;
+    }
+
+
   }
+
 
   return 0;
 }
