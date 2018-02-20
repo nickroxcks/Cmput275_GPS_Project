9053b54365dbd36eeaf38734670e85409064662d
mmcevoy93 <mmcevoy@ualberta.ca>
2018-02-20 14:08:21 -0700

    save early save often

--- server/python_talk.py
+++ server/python_talk.py
@@ -1,48 +1,43 @@
 from serial import Serial
 from time import sleep
-# screen /dev/cu.usbmodemFD121
+
+temp = ["W 5365488 -11333914",
+        'W 5365238 -11334423',
+        'W 5365157 -11334634',
+        'W 5365035 -11335026',
+        'W 5364789 -11335776',
+        'W 5364774 -11335815',
+        'W 5364756 -11335849',
+        'W 5364727 -11335890']
+
 if __name__ == "__main__":
     # timeout is in seconds, can specify a float like 4.5
-    received = []
-    with Serial("/dev/tty.usbmodemFA131", baudrate=9600, timeout=5) as ser:
+    with Serial("/dev/ttyACM0", baudrate=9600, timeout=5) as ser:
         iteration = 0
-        path = 8 # temp
         while True:
-            # infinite loop that echoes all messages from
-            # the arduino to the terminal
             line = ser.readline()
-
             if not line:
                 print("timeout, restarting...")
                 continue
-
             line_string = line.decode("ASCII")
-
             stripped = line_string.rstrip("\r\n")
             print("I read line: ", stripped)
-
-            # construct the line you want to print to the
-            # Arduino, don't forget the newline
-            out_line = "Iteration " + str(iteration) + "\n"
-            iteration += 1
-
-            encoded = out_line.encode("ASCII")
-            # now encoded is a byte object we can
-            # write to the arduino
-
-            ser.write(encoded)
-
-            # rest a bit between rounds of communication
-            sleep(2)
-
-            stripped.strip()
             if stripped[0] == 'R':
-                received.append = stripped[1]
-                received.append = stripped[2]
-                received.append = stripped[3]
-                received.append = stripped[4]
-                encoded = 'N' + len(path)
+                a = "N 8 \n"
+                encoded = a.encode("ASCII")
+                ser.write(encoded)
+                continue
+            elif stripped[0] == 'A':
+                if temp:
+                    a = temp.pop(0)
+                else:
+                    a = "E \n"
+                encoded = a.encode("ASCII")
+                ser.write(encoded)
+                continue
+            else:
+                out_line = "Iteration " + str(iteration) + "\n"
+                iteration += 1
                 encoded = out_line.encode("ASCII")
                 ser.write(encoded)
-            if stripped[0] == 'A':
-                path = 'W' + path
+            sleep(2)
