952fd92185557b978e351bb2def2e54d1fd8ca30
cmput274 <cmput274@ualberta.ca>
2018-02-22 16:12:27 -0700

    update

--- client/test_talk_max/arduino_talk.cpp
+++ client/test_talk_max/arduino_talk.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 void setup() {
   init();
 
@@ -7,43 +6,55 @@ void setup() {
 }
 // TODO Communicationseems to work. just need to findout if the the correct values are being sent
 int main() {
-  enum State {Input_Ready, Sending_In, Waiting_N, Sending_A, Waiting_W, Ending};
+  enum State {Sending_In, Waiting_N, Sending_A, Waiting_W, Ending};
   State curr_state = Sending_In;
   setup();
   Serial.println("Starting");
-  char buffer[129];
-  int used = 0;
+  char buffer[501];
   int paths;
-  int output;
+  int N_path;
+  int W_lat_long;
+  int used = 0;
+  int check;
+  char lat[6];
+  char lon[7];
   while (true) {
     while (Serial.available() == 0);
+    if (curr_state == Ending){
+
+      Serial.write("End");
+      Serial.flush();
+
+      break;
+    }
+
+    if (curr_state == Waiting_W){
+      check = Serial.read();
+      if (check == 'W'){
+        W_lat_long = check;
+        Serial.write('A');
+        Serial.flush();
+      }
+      if (check == 'E'){
+        curr_state = Ending;
+      }
+    }
+
+    if (curr_state == Waiting_N){
+      N_path = Serial.read();
+      Serial.write(N_path);
+      if (N_path == 'p'){
+        Serial.write('A');
+        Serial.flush();
+        curr_state = Waiting_W;
+      }
+    }
     if (curr_state == Sending_In){
       Serial.println("R 5365486 -11333915 5364728 -11335891"); //Put input here
       Serial.flush();
       curr_state = Waiting_N;
     }
-    if (curr_state == Waiting_N){
-      buffer[used] = Serial.read();
-      if (buffer[0] == 'N'){
-        output = Serial.read();
-        Serial.write('A');
-        curr_state = Waiting_W;
-      }
-    }
-    if (curr_state == Waiting_W){
-      buffer[used] = Serial.read();
-      if (buffer[0] == 'W'){
-          Serial.write('A');
-      }
-      if (buffer[0] == 'E'){
-        curr_state = Ending;
-        Serial.flush();
-      }
-    }
-    if (curr_state == Ending){
 
-      break;
-    }
   }
   return 0;
 }
--- server/python_talk.py
+++ server/python_talk.py
@@ -2,17 +2,11 @@ from serial import Serial
 from time import sleep
 
 temp = ["W 5365488 -11333914",
-        'W 5365238 -11334423',
-        'W 5365157 -11334634',
-        'W 5365035 -11335026',
-        'W 5364789 -11335776',
-        'W 5364774 -11335815',
-        'W 5364756 -11335849',
-        'W 5364727 -11335890']
+        "W 5365238 -11334423"]
 
 if __name__ == "__main__":
     # timeout is in seconds, can specify a float like 4.5
-    with Serial("/dev/ttyACM0", baudrate=9600, timeout=5) as ser:
+    with Serial("/dev/ttyACM3", baudrate=9600, timeout=5) as ser:
         iteration = 0
         while True:
             line = ser.readline()
@@ -23,10 +17,16 @@ if __name__ == "__main__":
             stripped = line_string.rstrip("\r\n")
             print("I read line: ", stripped)
             if stripped[0] == 'R':
-                a = "N 8 \n"
-                encoded = a.encode("ASCII")
-                ser.write(encoded)
-                continue
+                if not temp:
+                    a = "N 0\n"
+                    encoded = a.encode("ASCII")
+                    ser.write(encoded)
+                    break
+                else:
+                    a = "N 8\n"
+                    encoded = a.encode("ASCII")
+                    ser.write(encoded)
+                    continue
             elif stripped[0] == 'A':
                 if temp:
                     a = temp.pop(0)
@@ -40,4 +40,5 @@ if __name__ == "__main__":
                 iteration += 1
                 encoded = out_line.encode("ASCII")
                 ser.write(encoded)
+
             sleep(2)
